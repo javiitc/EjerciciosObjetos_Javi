c6e5f5ba8fd615d0fd05b2f108b1653ef751d28a
Javi <itsjavii1610@gmail.com>
2026-02-10 13:26:59 +0100

    Acabe el ej1, siuuu

--- Java/src/Ej1/Main.java
+++ Java/src/Ej1/Main.java
@@ -1,15 +1,22 @@
 package Ej1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Pokemon pk1 = new Pokemon("Snorlax", "Normal", 1, true, 143);
-        Pokemon pk2 = new Pokemon("Garchomp", "Dragon / Tierra", 4, true, 445);
-        Pokemon pk3 = new Pokemon("Blaziken", "Fuego / Lucha", 3, true, 257);
-        Pokemon pk4 = new Pokemon("Dusclops", "Fantasma", 3, true, 356);
-        Pokemon pk5 = new Pokemon("Ferropaladín", "Hada / Lucha", 9, true, 1006);
-        Pokemon pk6 = new Pokemon("Greninja", "Agua / Siniestro", 6, true, 658);
+        Scanner sc = new Scanner(System.in);
 
+        Pokemon pk1 = new Pokemon("Snorlax", "Normal", 1, true, 143, "Munchlax");
+        Pokemon pk2 = new Pokemon("Garchomp", "Dragon / Tierra", 4, true, 445, "Gabite");
+        Pokemon pk3 = new Pokemon("Blaziken", "Fuego / Lucha", 3, true, 257, "Combusken");
+        Pokemon pk4 = new Pokemon("Dusclops", "Fantasma", 3, true, 356, "Duskull");
+        Pokemon pk5 = new Pokemon("Ferropaladín", "Hada / Lucha", 9, true, 1006, "No tiene evolución previa");
+        Pokemon pk6 = new Pokemon("Greninja", "Agua / Siniestro", 6, true, 658, "Frogadier");
 
+        System.out.println("Que pokemon de tu equipo quieres consultar en la pokedex?");
+        int eleccion = sc.nextInt();
+
+        Pokemon.consultarPokemon(eleccion, pk1, pk2, pk3, pk4, pk5, pk6);
     }
 }
--- Java/src/Ej1/Pokemon.java
+++ Java/src/Ej1/Pokemon.java
@@ -7,12 +7,46 @@ public class Pokemon {
     int generacion;
     boolean capturado;
     int numPokedex;
+    String evolucionPrevia;
 
-    public Pokemon(String nombrePokemon, String tipoPokemon, int generacion, boolean capturado, int numPokedex) {
+    public Pokemon(String nombrePokemon, String tipoPokemon, int generacion, boolean capturado, int numPokedex, String evolucionPrevia) {
         this.nombrePokemon = nombrePokemon;
         this.tipoPokemon = tipoPokemon;
         this.generacion = generacion;
         this.capturado = capturado;
         this.numPokedex = numPokedex;
+        this.evolucionPrevia = evolucionPrevia;
+    }
+
+    public void infoPokemon () {
+        System.out.println(nombrePokemon);
+        System.out.println(tipoPokemon);
+        System.out.println("Generación: " + generacion);
+        System.out.println("Capturado? " + capturado);
+        System.out.println("Numero Pokedex Nacional: " + numPokedex);
+        System.out.println(evolucionPrevia);
+    }
+
+    public static void consultarPokemon (int eleccion, Pokemon pk1, Pokemon pk2, Pokemon pk3, Pokemon pk4, Pokemon pk5, Pokemon pk6) {
+        switch (eleccion) {
+            case 1:
+                pk1.infoPokemon();
+                break;
+            case 2:
+                pk2.infoPokemon();
+                break;
+            case 3:
+                pk3.infoPokemon();
+                break;
+            case 4:
+                pk4.infoPokemon();
+                break;
+            case 5:
+                pk5.infoPokemon();
+                break;
+            case 6:
+                pk6.infoPokemon();
+                break;
+        }
     }
 }
